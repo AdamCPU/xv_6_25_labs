2f57c5881c99f8417b446d8b97f1af01e0f9aba0
AdamCPU <adamalexiro@gmail.com>
2025-12-08 01:08:56 +0100

    uloha 2 cv 8 podla jokaya + miso

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -308,7 +308,7 @@ sys_open(void)
   int fd, omode;
   struct file *f;
   struct inode *ip;
-  int n;
+  int n, symlink_depth = 0;
 
   argint(1, &omode);
   if((n = argstr(0, path, MAXPATH)) < 0)
@@ -323,11 +323,27 @@ sys_open(void)
       return -1;
     }
   } else {
-    if((ip = namei(path)) == 0){
-      end_op();
-      return -1;
-    }
-    ilock(ip);
+    do {
+      if(symlink_depth >= 10 || (ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+        n = readi(ip, 0, (uint64)path, 0, MAXPATH-1);
+        if(n != ip->size){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        path[n] = '\0';
+        iunlockput(ip);
+        symlink_depth++;
+        continue;
+      }
+      break;
+    } while(1);
+
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -501,5 +517,31 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path [MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target)){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
